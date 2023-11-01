6635788b95a9d8801024a555a38290e39e57d0be
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-11-01 19:14:20 +0000

    python basic he setup

--- opencv_test.py
+++ opencv_test.py
@@ -1,1 +1,17 @@
+from __future__ import print_function
+import cv2 as cv
 
+src = cv.imread("./kamil.png")
+
+if src is None:
+    print('Image could not be opened at:\n./kamil.png')
+    exit(0)
+
+src = cv.cvtColor(src, cv.COLOR_RGB2GRAY)
+
+dst = cv.equalizeHist(src)
+
+cv.imshow('Source Image', src)
+cv.imshow('Source Image', dst)
+
+cv.waitKey()
