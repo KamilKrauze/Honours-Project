1e6f94a84ab396328fedee8be052c9cb51bbe8fa
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-11-07 17:31:11 +0000

    Clip test

--- adaptive_histogram_eq.py
+++ adaptive_histogram_eq.py
@@ -3,8 +3,6 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-# https://docs.opencv.org/4.x/d5/daf/tutorial_py_histogram_equalization.html
-
 # Application params
 display2user:bool = False
 
@@ -38,10 +36,21 @@ src = cv.imread(import_directory+"/"+file_import_list[0], cv.IMREAD_GRAYSCALE)
 
 heq = cv.equalizeHist(src)
 
-clahe = cv.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
-cl1 = clahe.apply(src)
+# Adaptive HE - https://docs.opencv.org/4.x/d5/daf/tutorial_py_histogram_equalization.html - 07/11/2023
+clahe1 = cv.createCLAHE(clipLimit=1.0, tileGridSize=(8,8))
+clahe2 = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+clahe3 = cv.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+clahe10 = cv.createCLAHE(clipLimit=10.0, tileGridSize=(8,8))
+
+cl1 = clahe1.apply(src)
+cl2 = clahe2.apply(src)
+cl3 = clahe3.apply(src)
+cl10 = clahe10.apply(src)
 
 cv.imshow("Source", src)
 cv.imshow("HE", heq)
-cv.imshow("AHE", cl1)
+cv.imshow("AHE - 1", cl1)
+cv.imshow("AHE - 2", cl2)
+cv.imshow("AHE - 3", cl3)
+cv.imshow("AHE - 10", cl10)
 cv.waitKey()
