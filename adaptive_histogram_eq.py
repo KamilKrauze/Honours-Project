3065c5b8450816621b8392651e6159facb4867c0
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-11-07 17:56:01 +0000

    Added a loop for all frames

--- adaptive_histogram_eq.py
+++ adaptive_histogram_eq.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Application params
-display2user:bool = False
+display2user:bool = True
 
 import_directory:str = "./exports/dicom-data"
 import_file:str = "list.txt"
@@ -32,25 +32,31 @@ except:
     print("File not found...")
     print("Directory or file may not exist")
     
-src = cv.imread(import_directory+"/"+file_import_list[0], cv.IMREAD_GRAYSCALE)
+for image in file_import_list:
+    src = cv.imread(import_directory+"/"+image, cv.IMREAD_GRAYSCALE)
+    if src is None:
+        print("File not found at:\n" + import_directory+"/"+image)
+    else:
+        heq = cv.equalizeHist(src)
 
-heq = cv.equalizeHist(src)
+        kernel_size = (8,8) # Too large of a kernel and all detail or entire image can be lost. Stay well below 32.
+        # Adaptive HE - https://docs.opencv.org/4.x/d5/daf/tutorial_py_histogram_equalization.html - 07/11/2023
+        clahe1 = cv.createCLAHE(clipLimit=1.0, tileGridSize=kernel_size)
+        clahe2 = cv.createCLAHE(clipLimit=2.0, tileGridSize=kernel_size)
+        clahe3 = cv.createCLAHE(clipLimit=3.0, tileGridSize=kernel_size)
+        clahe10 = cv.createCLAHE(clipLimit=10.0, tileGridSize=kernel_size)
 
-# Adaptive HE - https://docs.opencv.org/4.x/d5/daf/tutorial_py_histogram_equalization.html - 07/11/2023
-clahe1 = cv.createCLAHE(clipLimit=1.0, tileGridSize=(8,8))
-clahe2 = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-clahe3 = cv.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
-clahe10 = cv.createCLAHE(clipLimit=10.0, tileGridSize=(8,8))
+        cl1 = clahe1.apply(src)
+        cl2 = clahe2.apply(src)
+        cl3 = clahe3.apply(src)
+        cl10 = clahe10.apply(src)
 
-cl1 = clahe1.apply(src)
-cl2 = clahe2.apply(src)
-cl3 = clahe3.apply(src)
-cl10 = clahe10.apply(src)
-
-cv.imshow("Source", src)
-cv.imshow("HE", heq)
-cv.imshow("AHE - 1", cl1)
-cv.imshow("AHE - 2", cl2)
-cv.imshow("AHE - 3", cl3)
-cv.imshow("AHE - 10", cl10)
-cv.waitKey()
+        if display2user:
+            print(image)
+            cv.imshow("Source", src)
+            cv.imshow("HE", heq)
+            cv.imshow("AHE - 1", cl1)
+            cv.imshow("AHE - 2", cl2) # Seems to do better
+            cv.imshow("AHE - 3", cl3)
+            cv.imshow("AHE - 10", cl10)
+            cv.waitKey()
