5609e0dd433fb12326a96c36c7bc89d4c9207f8e
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-11-16 09:19:29 +0000

    Comparing contrast plotting

---
+++ compare_measures.py
@@ -0,0 +1,57 @@
+import numpy as npy
+import matplotlib.pyplot as plt
+
+# Custom modules
+from modules import ioUtilties as IO
+
+SELECTION:int = 0
+
+TITLE = ["Contrast Improvement Index", "Peak Signal to Pulse Ratio", "Structural Similarity Index Measure"]
+
+HEQ_SRC = [
+    "./exports/contrast-measures/heq/ciis.txt",
+    "./exports/contrast-measures/heq/psnr.txt",
+    "./exports/contrast-measures/heq/ssim.txt"
+]
+
+ADAP_HEQ_CL2_SRC = [
+    "./exports/contrast-measures/adap-heq/cl2/ciis.txt",
+    "./exports/contrast-measures/adap-heq/cl2/psnr.txt",
+    "./exports/contrast-measures/adap-heq/cl2/ssim.txt"
+]
+
+ADAP_HEQ_CL3_SRC = [
+    "./exports/contrast-measures/adap-heq/cl3/ciis.txt",
+    "./exports/contrast-measures/adap-heq/cl3/psnr.txt",
+    "./exports/contrast-measures/adap-heq/cl3/ssim.txt"
+]
+
+ADAP_HEQ_CL10_SRC = [
+    "./exports/contrast-measures/adap-heq/cl10/ciis.txt",
+    "./exports/contrast-measures/adap-heq/cl10/psnr.txt",
+    "./exports/contrast-measures/adap-heq/cl10/ssim.txt"
+]
+
+heq = IO.readFileToList( HEQ_SRC[SELECTION] )
+adap_heq_cl2 = IO.readFileToList( ADAP_HEQ_CL2_SRC[SELECTION] )
+adap_heq_cl3 = IO.readFileToList( ADAP_HEQ_CL3_SRC[SELECTION] )
+adap_heq_cl10 = IO.readFileToList( ADAP_HEQ_CL10_SRC[SELECTION] )
+
+heq = list( map(npy.float64, heq) )
+adap_heq_cl2 = list( map(npy.float64, adap_heq_cl2) )
+adap_heq_cl3 = list( map(npy.float64, adap_heq_cl3) )
+adap_heq_cl10 = list( map(npy.float64, adap_heq_cl10) )
+
+x_axis = range(0, len(heq))
+
+plt.title(TITLE[SELECTION] + " per frame")
+plt.xlabel("Frames")
+plt.ylabel(TITLE[SELECTION])
+
+plt.plot(heq, label="Histogram Equalization")
+plt.plot(x_axis, adap_heq_cl2, label="Adaptive Histogram Equalization - Clipping Limit = 2")
+plt.plot(x_axis, adap_heq_cl3, label="Adaptive Histogram Equalization - Clipping Limit = 3")
+plt.plot(x_axis, adap_heq_cl10, label="Adaptive Histogram Equalization - Clipping Limit = 10")
+
+plt.legend()
+plt.show()
