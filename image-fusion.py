7ce09c69d30d66e37fae847f6ce5ea05978a8e9a
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-01-22 17:58:19 +0000

    Select max and simple weighted

--- image-fusion.py
+++ image-fusion.py
@@ -22,7 +22,8 @@ def select_max(img1: cv.Mat, img2: cv.Mat) -> cv.Mat:
     if (img1.shape != img2.shape):
         raise  SystemExit("ERROR: Dimensions aren't the same")
     
-    output:cv.Mat = npy.zeros((512, 512, 3), npy.uint8)
+    # Create blank white image of the same size as input.
+    output:cv.Mat = npy.zeros((img1.shape[0], img1.shape[1], 3), npy.uint8)
     output[:,:,0] = 0xFF # Blue channel
     output[:,:,1] = 0xFF # Green channel
     output[:,:,2] = 0xFF # Red channel
@@ -38,15 +39,42 @@ def select_max(img1: cv.Mat, img2: cv.Mat) -> cv.Mat:
     
     return output
 
+def simple_weighted_avg( img1:cv.Mat, img2:cv.Mat, weight: float):
+    
+    if (img1 is None or img2 is None):
+        raise SystemExit("ERROR: One or both images are NULL")
+    
+    if (img1.shape != img2.shape):
+        raise  SystemExit("ERROR: Dimensions aren't the same")
+            
+    fused_image:cv.Mat = cv.addWeighted(img1, weight, img2, 1-weight, 0)
 
+    # Convert to uint8 if necessary
+    fused_image = npy.clip(fused_image, 0, 255).astype(npy.uint8)    
+    return fused_image
 
 if __name__ == "__main__":
-    src = cv.imread("./exports/opencv/adaptive-histogram-eq/cl3_frame25.png", cv.IMREAD_GRAYSCALE)
-    enh = cv.imread("./exports/opencv/adaptive-histogram-eq/cl85_frame25.png", cv.IMREAD_GRAYSCALE)
-    fused = select_max(src, enh)
+    src = cv.imread("./exports/opencv/adaptive-histogram-eq/cl2_frame25.png", cv.IMREAD_GRAYSCALE)
+    enh = cv.imread("./exports/opencv/adaptive-histogram-eq/cl3_frame25.png", cv.IMREAD_GRAYSCALE)
+
+    src = cv.GaussianBlur(src, (5,5), .9, None, .9)
+    enh = cv.GaussianBlur(enh, (5,5), 0)
+    
+    (ret, src) = cv.threshold(src, 90, 255, cv.THRESH_BINARY)
+    (ret, enh) = cv.threshold(enh, 70, 255, cv.THRESH_TOZERO)
+
+    
+    # thresh1 = cv.adaptiveThreshold(src, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 15, 0.05)
+    # thresh2 = cv.adaptiveThreshold(enh, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 9, 0.05)
+    
+    # fused = select_max(thresh1, thresh2)
+    
+    fused = simple_weighted_avg(src, enh, 0.5)
+
     
     # Side by side image concatenation - https://www.geeksforgeeks.org/how-to-display-multiple-images-in-one-window-using-opencv-python/ - Date Accessed: 14/12/2023
-    cv.imshow("Source Images", npy.concatenate((src,enh), axis=1))
+    cv.imshow("Source Images", npy.concatenate((src, enh), axis=1))
+    cv.imshow("Binarized Images", npy.concatenate((src, enh), axis=1))
     
     cv.imshow("Fused image", fused)
     
