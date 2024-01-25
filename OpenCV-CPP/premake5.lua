65d44389909c929d0088776c33d870541cb89f0d
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-01-25 12:50:28 +0000

    Added OpenCV console app project directory

--- OpenCV-CPP/premake5.lua
+++ OpenCV-CPP/premake5.lua
@@ -10,15 +10,15 @@ project "HonsProject-OpenCV-GUI"
     targetdir "bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
     files { 
-        "./src/**.h",
-        "./src/**.hpp",
-        "./src/**.c",
-        "./src/**.cpp"
+        "./GUI-Build/**.h",
+        "./GUI-Build/**.hpp",
+        "./GUI-Build/**.c",
+        "./GUI-Build/**.cpp"
     }
 
     includedirs {
         -- Project source code
-        "./src/",
+        "./GUI-Build/",
         
         -- Thirdparty
         "./include/"
@@ -49,3 +49,52 @@ project "HonsProject-OpenCV-GUI"
         optimize "On"
         libdirs { "./lib/" }
         links { "glfw3.lib", "opengl32.lib" }
+
+
+project "HonsProject-OpenCV-Console"
+    location "src"
+    kind "ConsoleApp"
+    language "C++"
+    
+    targetdir "bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    files { 
+        "./Console-Build/**.h",
+        "./Console-Build**.hpp",
+        "./Console-Build/**.c",
+        "./Console-Build/**.cpp"
+    }
+
+    includedirs {
+        -- Project source code
+        "./Console-Build/",
+        
+        -- Thirdparty
+        "./include/"
+    }
+
+-- Windows system
+
+    filter "system:windows"
+        system "windows"
+        cppdialect "C++17"
+        systemversion "latest"    
+
+-- Linux system
+    filter "system:linux"
+        system "linux"
+        cppdialect "gnu++17"
+        links { "m", "X11", "pthread", "Xi", "Xrandr", "dl" }
+
+-- Build configurations
+    filter "configurations:Debug"
+        defines "DEBUG"
+        symbols "On"
+        -- libdirs { "./lib/" }
+        -- links { "glfw3D.lib", "opengl32.lib" }
+
+    filter "configurations:Release"
+        defines "NDEBUG"
+        optimize "On"
+        -- libdirs { "./lib/" }
+        -- links { "glfw3.lib", "opengl32.lib" }
