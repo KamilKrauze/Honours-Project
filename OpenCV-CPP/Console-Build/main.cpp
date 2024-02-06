b5f793f4daa33d177b0e9e1f4fb9975c683de169
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-02-06 17:11:34 +0000

    HEQ test

--- OpenCV-CPP/Console-Build/main.cpp
+++ OpenCV-CPP/Console-Build/main.cpp
@@ -1,14 +1,19 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 #include <iostream>
 
 int main()
 {
     cv::Mat img = cv::imread("../../exports/dicom-data/frame00.png", cv::IMREAD_GRAYSCALE);
-
     std::cout << img.rows << "\n" << img.cols << "\n";
+
+    cv::Mat enh;
+    cv::equalizeHist(img, enh);
+
     cv::imshow("Source: Frame 0", img);
+    cv::imshow("Enhanced: Frame 0", enh);
     cv::waitKey(0);
 
     return 0;
