392f2345c669a92d5fe0d4c8503424e7b5013dea
Kamil Krauze <72396466+KamilKrauze@users.noreply.github.com>
2024-03-14 11:59:33 +0000

    Add files via upload

--- OpenCV-CPP/GUI-Build/main.cpp
+++ OpenCV-CPP/GUI-Build/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 #include "Core/Application.h"
 #include "Core/MediaManager.h"
@@ -12,12 +13,25 @@ int main()
 {
     Application app(1200, 800, "Honours Project");
 
-    std::vector<cv::String> list = {
-        "../../exports/dicom-data/frame25.png",
-        "../../exports/dicom-data/frame01.png"
-    };
+    std::vector<cv::String> list;
 
-    MediaManager::Get().load_images(list.begin(), list.end());
+    cv::String directory = "../../exports/dicom-data/";
+    cv::String file_format = ".png";
+
+    for (size_t i = 0; i <= 60; i++)
+    {
+        cv::String filename = "frame";
+        if (i < 10)
+        {
+            filename = filename + "0" + std::to_string(i);
+        }
+        else {
+            filename = filename + std::to_string(i);
+        }
+        list.push_back(directory + filename +file_format);
+    }
+
+    MediaManager::Get().load_images("src1", list.begin(), list.end());
 
     return app.run();
 }
