8636ed8e8f179e3820a67cc7f4c81490d9922b22
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-03-15 10:35:37 +0000

    Added SSIM

--- OpenCV-CPP/GUI-Build/Core/ImageMeasures.cpp
+++ OpenCV-CPP/GUI-Build/Core/ImageMeasures.cpp
@@ -19,6 +19,60 @@ static uint8_t maxOfMatrix(const cv::Mat& mat)
     return max;
 }
 
+static uint8_t meanOfMatrix(const cv::Mat& mat)
+{
+    const std::pair<size_t, size_t> SIZE = { mat.rows, mat.cols };
+    uint8_t pixel_sum = 0;
+    for (size_t i = 0; i < SIZE.first; i++)
+    {
+        for (size_t j = 0; j < SIZE.second; j++)
+        {
+            pixel_sum += mat.data[i * (mat.cols + j)];
+        }
+    }
+
+    return pixel_sum / (mat.rows * mat.cols);
+}
+
+static double varianceOfMatrix(const cv::Mat& mat, double mean)
+{
+    const std::pair<size_t, size_t> SIZE = { mat.rows, mat.cols };
+
+    double summation = 0.0f;
+    for (size_t i = 0; i < SIZE.first; i++)
+    {
+        for (size_t j = 0; j < SIZE.second; j++)
+        {
+            summation += std::pow(mat.data[i + (SIZE.first * j)], 2);
+        }
+    }
+
+    return summation / (SIZE.first * SIZE.second);
+}
+
+static double covarianceOfMatrix(const cv::Mat& org, const cv::Mat& enh)
+{
+    const std::pair<size_t, size_t> ORG_SIZE = { org.rows, org.cols };
+    const std::pair<size_t, size_t> ENH_SIZE = { enh.rows, enh.cols };
+
+    const uint8_t ORG_MEAN = meanOfMatrix(org);
+    const uint8_t ENH_MEAN = meanOfMatrix(enh);
+
+    const auto totalPixels = (ENH_SIZE.first * ENH_SIZE.second) - 1;
+
+    double summation = 0.0f;
+    for (size_t i = 0; i < ENH_SIZE.first; i++)
+    {
+        for (size_t j = 0; j < ENH_SIZE.second; j++)
+        {
+            summation += (org.data[i + (ORG_SIZE.first * j)] - ORG_MEAN)
+                * (enh.data[i + (ENH_SIZE.first * j)] - ENH_MEAN);
+        }
+    }
+
+    return summation / totalPixels;
+}
+
 double ImgMeasure::PSNR(const cv::Mat& org, const cv::Mat& enh)
 {
     const std::pair<size_t, size_t> ORG_SIZE = { org.rows, org.cols  };
@@ -47,4 +101,28 @@ double ImgMeasure::PSNR(const cv::Mat& org, const cv::Mat& enh)
     double fraction = std::pow(L_1, 2) / ( summation_x_y / N);
 
     return 10 * std::log10(fraction);
+}
+
+double ImgMeasure::SSIM(const cv::Mat& org, const cv::Mat& enh)
+{
+    const uint8_t ORG_MEAN = meanOfMatrix(org);
+    const uint8_t ENH_MEAN = meanOfMatrix(enh);
+
+    const double ORG_VARIANCE = varianceOfMatrix(org, ORG_MEAN);
+    const double ENH_VARIANCE = varianceOfMatrix(enh, ENH_MEAN);
+
+    const double COVARIANCE = std::sqrt(covarianceOfMatrix(org, enh));
+
+    const uint8_t L = maxOfMatrix(enh);
+    const double k1 = 0.01f;
+    const double c1 = std::pow(k1 * L, 2);
+
+    const double k2 = 0.03f;
+    const double c2 = std::pow(k2 * L, 2);
+
+    const double numerator = (2 * ORG_MEAN * ENH_MEAN + c1) * (2 * COVARIANCE + c2);
+    const double denominator = (std::pow(ORG_MEAN, 2) + std::pow(ENH_MEAN, 2) + c1)
+        + (ORG_VARIANCE + ENH_VARIANCE + c2);
+
+    return numerator / denominator;
 }
