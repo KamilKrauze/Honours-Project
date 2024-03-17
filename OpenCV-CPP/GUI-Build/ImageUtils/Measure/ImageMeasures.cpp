ca198e6fa3fc591f62d3eb44aaffb44ffdcafd0f
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-03-17 12:13:01 +0000

    Moved math functions and measure around

---
+++ OpenCV-CPP/GUI-Build/ImageUtils/Measure/ImageMeasures.cpp
@@ -0,0 +1,92 @@
+#include "ImageUtils/Measure/ImageMeasures.hpp"
+#include "ImageUtils/Math/ImageMath.hpp"
+
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <thread>
+
+double ImgMeasure::PSNR(const cv::Mat& org, const cv::Mat& enh)
+{
+    const std::pair<size_t, size_t> ORG_SIZE = { org.rows, org.cols  };
+    const std::pair<size_t, size_t> ENH_SIZE = { enh.rows, enh.cols  };
+
+    assert(ORG_SIZE == ENH_SIZE);
+
+    // (L_1) - The max intensity of the image
+    const uint8_t L_1 = ImgMath::maxOfMatrix(enh);
+
+    // Total number of pixels
+    const size_t N = ENH_SIZE.first * ENH_SIZE.second;
+
+    double summation_x_y = 0;
+#pragma omp parallel for
+    for (size_t i = 0; i < ENH_SIZE.first; i++)
+    {
+        for (size_t j = 0; j < ENH_SIZE.second; j++)
+        {
+            int pixel = org.data[i * (ORG_SIZE.second + j)] - enh.data[i * (ENH_SIZE.second + j)];
+            pixel = std::pow(std::abs(pixel), 2);
+            summation_x_y += pixel;
+        }
+    }
+
+    double fraction = std::pow(L_1, 2) / ( summation_x_y / N);
+
+    return 10 * std::log10(fraction);
+}
+
+double ImgMeasure::SSIM(const cv::Mat& org, const cv::Mat& enh)
+{
+    const uint8_t ORG_MEAN = ImgMath::meanOfMatrix(org);
+    const uint8_t ENH_MEAN = ImgMath::meanOfMatrix(enh);
+
+    const double ORG_VARIANCE = ImgMath::varianceOfMatrix(org, ORG_MEAN);
+    const double ENH_VARIANCE = ImgMath::varianceOfMatrix(enh, ENH_MEAN);
+
+    const double COVARIANCE = std::sqrt(ImgMath::covarianceOfMatrix(org, enh));
+
+    const uint8_t L = ImgMath::maxOfMatrix(enh);
+    const double k1 = 0.01f;
+    const double c1 = std::pow(k1 * L, 2);
+
+    const double k2 = 0.03f;
+    const double c2 = std::pow(k2 * L, 2);
+
+    const double numerator = (2 * ORG_MEAN * ENH_MEAN + c1) * (2 * COVARIANCE + c2);
+    const double denominator = (std::pow(ORG_MEAN, 2) + std::pow(ENH_MEAN, 2) + c1)
+        + (ORG_VARIANCE + ENH_VARIANCE + c2);
+
+    return numerator / denominator;
+}
+
+inline static void printLocalContrasts(const std::vector<double>& vec)
+{
+    for (auto& item : vec)
+    {
+        printf("%Lf \n", item);
+    }
+    return;
+}
+
+double ImgMeasure::CII(const cv::Mat& org, const cv::Mat& enh)
+{
+    // Local contrasts
+    std::vector<double> Cloc_org(0), Cloc_enh(0);
+    double entropy_org = 0.0f, entropy_enh = 0.0f;
+
+    std::thread t1([&org, &Cloc_org, &entropy_org] {
+        Cloc_org = ImgMath::coefficientsOfLocation(org);
+        entropy_org = ImgMath::entropy(Cloc_org);
+        });
+
+    std::thread t2([&enh, &Cloc_enh, &entropy_enh] {
+        Cloc_enh = ImgMath::coefficientsOfLocation(enh);
+        entropy_enh = ImgMath::entropy(Cloc_enh);
+        });
+
+    t1.join();
+    t2.join();
+
+    return entropy_enh / entropy_org;
+}
--- OpenCV-CPP/GUI-Build/main.cpp
+++ OpenCV-CPP/GUI-Build/main.cpp
@@ -12,44 +12,27 @@
 
 int main()
 {
-    cv::Mat img = cv::imread("../../exports/dicom-data/frame00.png");
-    cv::Mat enh = cv::imread("../../exports/opencv/adaptive-histogram-eq/cl2_frame00.png");
+    Application app(1200, 800, "Honours Project");
 
-    cv::imshow("src", img);
-    cv::imshow("enh", enh);
+    std::vector<cv::String> list;
 
-    double min = 0, max = 0;
-    cv::minMaxLoc(img, &min, &max);
-    std::cout << min << "\n" << max << "\n";
+    cv::String directory = "../../exports/dicom-data/";
+    cv::String file_format = ".png";
 
-    std::cout << ImgMeasure::PSNR(img, enh) << "\n";
-    std::cout << ImgMeasure::SSIM(img, enh) << "\n";
-    std::cout << ImgMeasure::CII(img, enh) << "\n";
+    for (size_t i = 0; i <= 60; i++)
+    {
+        cv::String filename = "frame";
+        if (i < 10)
+        {
+            filename = filename + "0" + std::to_string(i);
+        }
+        else {
+            filename = filename + std::to_string(i);
+        }
+        list.push_back(directory + filename +file_format);
+    }
 
-    cv::waitKey(0);
-    return 0;
+    MediaManager::Get().load_images("src1", list.begin(), list.end());
 
-    //Application app(1200, 800, "Honours Project");
-
-    //std::vector<cv::String> list;
-
-    //cv::String directory = "../../exports/dicom-data/";
-    //cv::String file_format = ".png";
-
-    //for (size_t i = 0; i <= 60; i++)
-    //{
-    //    cv::String filename = "frame";
-    //    if (i < 10)
-    //    {
-    //        filename = filename + "0" + std::to_string(i);
-    //    }
-    //    else {
-    //        filename = filename + std::to_string(i);
-    //    }
-    //    list.push_back(directory + filename +file_format);
-    //}
-
-    //MediaManager::Get().load_images("src1", list.begin(), list.end());
-
-    //return app.run();
+    return app.run();
 }
