9fa0537dc239bc34e04d4cd79aa1e3f68586973d
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-04-05 21:08:58 +0100

    Updated for batch image processing and export

--- OpenCV-Python/dwt_pca_img_fusion.py
+++ OpenCV-Python/dwt_pca_img_fusion.py
@@ -3,6 +3,8 @@ import pywt
 import numpy as npy
 import scipy as spy
 
+from rich.progress import track as prog_bar
+
 class DWT_Coeffs:
     LL: npy.ndarray # Approximate coeffs   (low-pass)
     LH: npy.ndarray # Horizontal coeffs    (high-pass)
@@ -43,16 +45,11 @@ def fuse_coefficients(cf1:npy.ndarray, cf2:npy.ndarray) -> npy.ndarray:
     C = npy.cov(X, rowvar=False)
     vals, vecs = spy.linalg.eigh(C) # Eigen Valeus, Eigen Vectors
     vals = vals.T.reshape(-1, 1)
-        
-    print(vals)
     
     if vals[0] >= vals[1]:
         P = vecs[0] / npy.sum(vecs[:,0])
     else:
         P = vecs[1] / npy.sum(vecs[:,1])
-
-    print(P)
-    print()
 
     # Fuse coefficients based on weights
     fused_cf = (P[0] * cf1) + (P[1] * cf2)
@@ -71,20 +68,47 @@ def PCA_onDWT(img1: cv.Mat, img2: cv.Mat) -> cv.Mat:
     LL = fuse_coefficients(coeff1.LL, coeff2.LL) # Approximation
     LH = fuse_coefficients(coeff1.LH, coeff2.LH) # Horizontal
     HL = fuse_coefficients(coeff1.HL, coeff2.HL) # Vertical
-    HH = fuse_coefficients(coeff1.HH, coeff2.HH)
+    # HH = fuse_coefficients(coeff1.HH, coeff2.HH) # Diagonal
 
     # Perform Inverse DWT 2D and convert to OpenCV Matrix
     return cv.convertScaleAbs(pywt.idwt2( (LL, (LH, HL, None)), wavelet='db4'))
 
 if "__main__" == __name__:
-    img1 = cv.imread('./exports/opencv/adaptive-histogram-eq/cl2_frame25.png', cv.IMREAD_GRAYSCALE)
-    img2 = cv.imread('./exports/opencv/adaptive-histogram-eq/cl10_frame25.png', cv.IMREAD_GRAYSCALE)
+    # img1 = cv.imread('./exports/opencv/adaptive-histogram-eq/cl2_frame25.png', cv.IMREAD_GRAYSCALE)
+    # img2 = cv.imread('./exports/opencv/adaptive-histogram-eq/cl10_frame25.png', cv.IMREAD_GRAYSCALE)
     
-    fused = PCA_onDWT(img1, img2)
+    # fused = PCA_onDWT(img1, img2)
 
-    cv.imshow("Img 1", img1)
-    cv.imshow("Img 2", img2)
-    cv.imshow("Fused", fused)
+    # cv.imshow("Img 1", img1)
+    # cv.imshow("Img 2", img2)
+    # cv.imshow("Fused", fused)
 
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+    
+    fused:cv.Mat = list()
+    
+    file_ext:str = ".png"
+    fp1:str = "./exports/opencv/adaptive-histogram-eq/cl2_frame"
+    fp2:str = "./exports/opencv/adaptive-histogram-eq/cl4_frame"
+    
+    export_fp = "./exports/weighted_pca_dwt/cl2_cl4/"
+    export_name = "cl2_cl4_frame"
+    
+    for i in prog_bar(range(61), description="Computing Weighted-PCA-DWT Image Fusion..."):
+        number:str = str(i)
+        if (i < 10):
+            number = "0" + str(i)
+        
+        img1:cv.Mat = cv.imread((fp1+number+file_ext), cv.IMREAD_GRAYSCALE)
+        img2:cv.Mat = cv.imread((fp2+number+file_ext), cv.IMREAD_GRAYSCALE)
+        
+        fused_image:cv.Mat = PCA_onDWT(img1, img2)
+        fused.append(fused_image)
+        
+    for i in prog_bar(range(61), description="Exporting..."):
+        number:str = str(i)
+        if (i < 10):
+            number = "0" + str(i)
+        path:str=export_fp + export_name + number + file_ext
+        cv.imwrite(path, fused[i])
