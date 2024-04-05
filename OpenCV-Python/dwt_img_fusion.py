527909be9c80fd08e8182294a4f9be3f8bd48976
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-04-05 20:56:58 +0100

    Updated to do batch image processing

--- OpenCV-Python/dwt_img_fusion.py
+++ OpenCV-Python/dwt_img_fusion.py
@@ -2,6 +2,8 @@ import cv2 as cv
 import pywt
 import numpy as npy
 
+from rich.progress import track as prog_bar
+
 # Loosley based-off the MATLAB example, ported to Python myself
 # https://www.youtube.com/watch?v=cDFJzrlm_U0
 # 21/02/2024
@@ -31,20 +33,40 @@ def fuse_images(img1:cv.Mat, img2:cv.Mat, wt:str) -> cv.Mat:
             LL[i,j] = npy.max([LL1[i,j], LL2[i,j]])
             LH[i,j] = npy.max([LH1[i,j], LH2[i,j]])
             HL[i,j] = npy.max([HL1[i,j], HL2[i,j]])
-            HH[i,j] = npy.max([HH1[i,j], HH2[i,j]])
+            # HH[i,j] = npy.max([HH1[i,j], HH2[i,j]])
 
-    reconstructed = pywt.idwt2( (LL, (LH, HL, HH)), wavelet=wt)
+    reconstructed = pywt.idwt2( (LL, (LH, HL, None)), wavelet=wt)
     
     return cv.convertScaleAbs(reconstructed)
 
-if __name__ == "__main__":
-    img1:cv.Mat = cv.imread("../exports/opencv/adaptive-histogram-eq/cl2_frame25.png", cv.IMREAD_GRAYSCALE)
-    img2:cv.Mat = cv.imread("../exports/opencv/adaptive-histogram-eq/cl4_frame25.png", cv.IMREAD_GRAYSCALE)
+if __name__ == "__main__":  
+    # img1:cv.Mat = cv.imread("./exports/opencv/adaptive-histogram-eq/cl2_frame25.png", cv.IMREAD_GRAYSCALE)
+    # img2:cv.Mat = cv.imread("./exports/opencv/adaptive-histogram-eq/cl5_frame25.png", cv.IMREAD_GRAYSCALE)
+    # fused_img = fuse_images(img1, img2, wt='db4')
     
-    fused_img = fuse_images(img1, img2, wt='db4')
+    fused:cv.Mat = list()
     
-    cv.imshow("Img - 1", img1)
-    cv.imshow("Img - 2", img2)
+    file_ext:str = ".png"
+    fp1:str = "./exports/opencv/adaptive-histogram-eq/cl2_frame"
+    fp2:str = "./exports/opencv/adaptive-histogram-eq/cl4_frame"
     
-    cv.imshow("Fused", fused_img)
-    cv.waitKey(0)
+    export_fp = "./exports/dwt/cl2_cl10_fusion_db4/"
+    export_name = "cl2_cl10_frame"
+    
+    for i in prog_bar(range(61), description="Computing DWT Image Fusion..."):
+        number:str = str(i)
+        if (i < 10):
+            number = "0" + str(i)
+        
+        img1:cv.Mat = cv.imread((fp1+number+file_ext), cv.IMREAD_GRAYSCALE)
+        img2:cv.Mat = cv.imread((fp2+number+file_ext), cv.IMREAD_GRAYSCALE)
+        
+        fused_image:cv.Mat = fuse_images(img1, img2, wt='db4')
+        fused.append(fused_image)
+        
+    for i in prog_bar(range(61), description="Exporting..."):
+        number:str = str(i)
+        if (i < 10):
+            number = "0" + str(i)
+        path:str=export_fp + export_name + number + file_ext
+        cv.imwrite(path, fused[i])
